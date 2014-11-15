6a8f793c9180483acdfcced68fc5179dddf3b961
Jason A Smith <jas7553@gmail.com>
2014-11-15 10:42:53 -0800

    Wire the SimpleCollectionViewViewController to the storyboard

--- UICollectionView Example/SimpleCollectionViewViewController.h
+++ UICollectionView Example/SimpleCollectionViewViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SimpleCollectionViewViewController : UIViewController
+@interface SimpleCollectionViewViewController : UICollectionViewController<UICollectionViewDataSource>
 
 
 @end
--- UICollectionView Example/SimpleCollectionViewViewController.m
+++ UICollectionView Example/SimpleCollectionViewViewController.m
@@ -24,4 +24,9 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - IBActions
+
+- (IBAction)addCell:(id)sender {
+}
+
 @end

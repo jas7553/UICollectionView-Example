393bed90d485b39b1eb69c0272c0a54175697900
Jason A Smith <jas7553@gmail.com>
2014-11-15 10:56:15 -0800

    Implement SimpleCollectionViewViewController
    Implement the underlying model (an NSInteger), the init method,
    the UICollectionViewDataSource methods, and the addCell IBAction.

--- UICollectionView Example/SimpleCollectionViewViewController.m
+++ UICollectionView Example/SimpleCollectionViewViewController.m
@@ -10,23 +10,62 @@
 
 @interface SimpleCollectionViewViewController ()
 
+/*
+ * Number of cells to begin with.
+ * This will be incremented and decremented when cells are added and deleted.
+ */
+@property NSInteger cellCount;
+
 @end
 
 @implementation SimpleCollectionViewViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.cellCount = 3;
+    }
+    return self;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - UICollectionViewDataSource
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return self.cellCount;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell* cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell"
+                                                                           forIndexPath:indexPath];
+    cell.contentView.backgroundColor = [UIColor lightTextColor];
+    
+    return cell;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    // A cell has been tapped, so delete it
+    [self removeItemAtIndex:indexPath];
+}
+
+#pragma mark - Private methods
+
+- (void)removeItemAtIndex:(NSIndexPath *)indexPath {
+    self.cellCount -= 1;
+    
+    [self.collectionView deleteItemsAtIndexPaths:@[indexPath]];
 }
 
 #pragma mark - IBActions
 
 - (IBAction)addCell:(id)sender {
+    self.cellCount += 1;
+    
+    NSIndexPath *indexPath = [NSIndexPath indexPathForItem:self.cellCount - 1 inSection:0];
+    [self.collectionView insertItemsAtIndexPaths:@[indexPath]];
 }
 
 @end
